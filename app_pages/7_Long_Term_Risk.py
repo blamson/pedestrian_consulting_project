0c7b819cbb8cf3dde09571dc9df787feace78086
Brady Lamson <lamsonbrady@gmail.com>
2026-05-07 15:10:44 -0600

    implemented default value tabs for long term risk

--- app_pages/7_Long_Term_Risk.py
+++ app_pages/7_Long_Term_Risk.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import polars as pl
 import plotly.express as px
-from risk_estimation import crash_frequency_helpers as risk_mod, streamlit_helpers as helpers
+from risk_estimation import crash_frequency_helpers as risk_mod, streamlit_helpers as helpers, plotting_helpers
 from loguru import logger
 
 title = "Long Term Risk"
@@ -17,6 +17,12 @@ spf_table = helpers.load_data("data/spfs.csv")
 intersection, inputs = helpers.build_sidebar(
     aadt_limit_table,
     include_years=True
+)
+
+# Load original results
+original_results = (
+    helpers.load_data("data/results/sweep_results_2026-05-07.csv")
+    .filter(pl.col("intersection_name") == intersection.name)
 )
 
 # Debug info
@@ -37,54 +43,21 @@ results_df = (
 text = helpers.load_text("app_pages/text_files/dashboard/long-term-risk.md")
 st.markdown(text)
 
-tab1, tab2 = st.tabs(["Chart - Selected Values", "Data"])
-col1, col2 = tab1.columns(2)
+tab1, tab2, tab3, tab4 = st.tabs([
+    "Chart - Selected Values",
+    "Chart - Default Values",
+    "Data - Selected Values",
+    "Data - Default Values"
+])
 
-fig = px.bar(
-    results_df.filter(pl.col("years") == inputs.years), 
-    x="scenario", 
-    y="long_run_ped_percent", 
-    text="long_run_ped_percent",
-    title=f"Chance of at least one accident in {inputs.years} years",
-    labels={
-        "scenario": "Scenario",
-        "long_run_ped_percent": "Chance of an accident (%)"
-    }
-)
-fig.update_traces(
-    textposition="outside",
-    texttemplate="%{text:.0f}%",
-    marker=dict(color=px.colors.qualitative.Vivid)
-)
-fig.update_layout(yaxis_range=[0, 100])
-col1.plotly_chart(fig, key="long_term_risk_plot")
+with tab1:
+    plotting_helpers.render_long_term_risk(results_df, "selected", inputs.years,)
 
-fig = px.line(
-    results_df, 
-    x="years", 
-    y="long_run_ped_percent", 
-    color="scenario",
-    title="Chance of at least one accident over time",
-    color_discrete_sequence=px.colors.qualitative.Vivid,
-    labels={
-        "years": "Years",
-        "scenario": "Scenario",
-        "long_run_ped_percent": "Chance of an accident (%)"
-    }
-)
-fig.add_vline(x=inputs.years, line_width=1, line_dash="dash", line_color="red", opacity=0.7, annotation_text=f"Year Selected: {inputs.years}")
-fig.update_traces(mode="markers+lines", hovertemplate=None)
-fig.update_layout(
-    yaxis_range=[0, 100],
-    legend=dict(
-        yanchor="bottom",
-        y=0.01,
-        xanchor="right",
-        x=0.99
-    ),
-    hovermode="x"
-)
-fig.update_xaxes(showspikes=True, spikemode="across", spikethickness=1, spikecolor="grey")
-col2.plotly_chart(fig, key="year_line_plot")
+with tab2:
+    plotting_helpers.render_long_term_risk(original_results, "default")
 
-tab2.write(results_df)
+with tab3:
+    st.write(results_df)
+
+with tab4:
+    st.write(original_results)
